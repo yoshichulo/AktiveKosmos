d9127584aa1a558b1463f7c1017596a89ced139b
Yoshi <joseangelyoshi@gmail.com>
2020-04-23 15:11:27 +0200

    Fixed menu design and colors

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,48 +1,41 @@
 <template>
   <v-container fluid class="pa-0">
     <!-- Social media bar -->
-    <v-container fluid class="d-flex justify-end black pa-0">
-      <v-col lg="1" md="1" sm="2" cols="3">
+    <v-container fluid class="d-flex justify-end pa-0" style="background-color: #ef9141">
+      <v-col lg="1" md="1" sm="3" cols="3">
         <v-row>
           <v-flex v-for="icon in socialNetworks" :key="icon.title">
-            <font-awesome-icon :icon="['fab', icon.icon]" size="lg" style="color: white" />
+            <font-awesome-icon :icon="['fab', icon.icon]" size="2x" style="color: white" />
           </v-flex>
         </v-row>
       </v-col>
     </v-container>
 
     <!-- Nav bar -->
-    <v-container fluid class="grey lighten-5 d-flex justify-center align-center nav-container">
-      <v-col lg="3" md="2" sm="2" cols="2">
-        <v-img :src="aktiveLogo" height="80px" contain></v-img>
-      </v-col>
-      <v-col lg="7" md="10" sm="10" cols="10" class="d-flex justify-center">
-        <v-row>
-            <v-menu open-on-hover bottom offset-y v-for="(link, index) in linksDropdown" :key="index">
-              <template v-slot:activator="{ on }">
-                <v-btn class="navbar-btn" text v-on="on">{{ link.title }} <v-icon>arrow_drop_down</v-icon></v-btn>
-              </template>
-
-              <v-list>
-                <v-list-item v-for="(item, index) in link.subs" :key="index" @click="">
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-        </v-row>
+    <v-container fluid class="grey lighten-5 d-flex justify-center align-center nav-container pa-0">
+      <v-col class="pa-0">
+        <v-col class="pt-10 pb-5">
+          <v-img :src="aktiveLogo" height="240px" contain></v-img>
+        </v-col>
+        <v-col class="d-flex justify-center py-8">
+          <v-container class="d-flex justify-center">
+            <nav-button v-for="(link, index) in linksDropdown" :key="index" :link="link"/>
+          </v-container>
+        </v-col>
       </v-col>
     </v-container>
   </v-container>
 </template>
 
 <script>
+  import NavButton from "./NavButton";
   export default {
     name: "NavBar",
+    components: {NavButton},
     data() {
       return {
-        aktiveLogo: require("assets/icons/aktive-text-right-small.png"),
+        aktiveLogo: require("assets/icons/aktive-orange-small.png"),
         socialNetworks: [
-          {"title": "Twitter", "icon": "twitter-square"},
           {"title": "Instagram", "icon": "instagram-square"},
           {"title": "Facebook", "icon": "facebook-square"},
         ],
@@ -91,17 +84,6 @@
 </script>
 
 <style scoped>
-  .navbar-btn {
-    font-size: 16px;
-    font-weight: bold;
-    transition: .2s;
-  }
-  .navbar-btn:hover {
-    background-color: #FAFAFA;
-    border-width: 0 0 2px 0;
-    border-style: solid;
-    border-color: black;
-  }
   .nav-container {
     box-shadow: 0px 10px 26px -10px rgba(0,0,0,0.75);
   }
---
+++ components/NavButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-menu open-on-hover bottom offset-y>
+    <template v-slot:activator="{ on }">
+      <div class="nav-button">
+        <a v-on="on">{{ link.title }}<v-icon>arrow_drop_down</v-icon></a>
+      </div>
+    </template>
+
+    <v-list>
+      <v-list-item v-for="(item, index) in link.subs" :key="index" @click="" class="nav-menu-option">
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+  export default {
+    name: "NavButton",
+    props: {
+      link: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .nav-button {
+    height: 100%;
+    justify-content: center;
+    text-transform: uppercase;
+    padding-left: 1%;
+    padding-right: 1%;
+  }
+
+  .nav-button a {
+    text-decoration: none;
+    padding-bottom: 3px;
+    color: black;
+  }
+
+  .nav-button a:hover {
+    color: orange;
+  }
+
+  .v-list-item--link:hover {
+    background-color: white;
+    color: orange;
+  }
+</style>
